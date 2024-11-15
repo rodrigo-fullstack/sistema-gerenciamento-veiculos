28fd98f98a701f36e6035a6c37eb6438e0c60f08
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-15 11:30:21 -0300

    feat (table): Atualização do script da tabela

--- app/sql/create-table.sql
+++ app/sql/create-table.sql
@@ -2,12 +2,4 @@
 CREATE DATABASE IF NOT EXISTS vehicle_management;
 USE vehicle_management;
 
-CREATE TABLE veiculos (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  marca varchar(255) NOT NULL,
-  modelo varchar(255) NOT NULL,
-  ano varchar(4) NOT NULL,
-  cor varchar(255) NOT NULL,
-  num_reg varchar(255) NOT NULL UNIQUE,
-  PRIMARY KEY (id)
-);
+CREATE TABLE `vehicle_management`.`veiculos` (`id` INT NOT NULL AUTO_INCREMENT , `marca` VARCHAR(255) NOT NULL , `modelo` VARCHAR(255) NOT NULL , `ano` VARCHAR(4) NOT NULL , `cor` VARCHAR(255) NOT NULL , `num_reg` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), UNIQUE
